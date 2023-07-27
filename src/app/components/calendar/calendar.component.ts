227ce0cbc4a7e2fb94c2cb80d6d0f278ae9436e0
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2023-07-27 15:47:47 +0300

    calendar events

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
-import { CalendarOptions, EventInput } from "@fullcalendar/core";
+import { CalendarOptions, DateSelectArg, EventInput } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
+
 @Component({
 	selector: "app-calendar",
 	templateUrl: "./calendar.component.html",
@@ -9,25 +10,19 @@ import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
 })
 export class CalendarComponent {
 	events: EventInput[] = [];
+
 	calendarOptions: CalendarOptions = {
 		initialView: "dayGridMonth",
 		plugins: [interactionPlugin, dayGridPlugin],
 		selectable: true,
-		events: this.events,
-		dateClick: (selectedDate: DateClickArg): void => {
-			// alert("You selected: " + selectedDate.date);
-			// console.log(selectedDate.date);
-			const event = { title: "Samuel Buraga", date: selectedDate.date };
-			this.events = [...this.events, event]; // Update events array
-			// Trigger the change detection manually to update the calendar events
-			this.calendarOptions.events = this.events;
-		},
-		select: (selectedRange) => {
-			alert(selectedRange.start + " to " + selectedRange.end);
-			const event = { title: "Samuel Buraga", start: selectedRange.start, end: selectedRange.end };
-			this.events = [...this.events, event]; // Update events array
-			// Trigger the change detection manually to update the calendar events
-			this.calendarOptions.events = this.events;
-		},
+		select: (selectedRange) => this.selectDates(selectedRange),
 	};
+
+	selectDates(selectedRange: DateSelectArg): void {
+		alert(selectedRange.start + " to " + selectedRange.end);
+		console.log(selectedRange);
+		const event = { title: "Samuel Buraga", start: selectedRange.start, end: selectedRange.end };
+		this.events = [...this.events, event];
+		this.calendarOptions.events = this.events;
+	}
 }
