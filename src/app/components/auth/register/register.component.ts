84f45e01719878d8c96e429fa2fd1d65f21d860a
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-02 17:29:21 +0300

    created login and register component, added routes

---
+++ src/app/components/auth/register/register.component.ts
@@ -0,0 +1,37 @@
+// register.component.ts
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    return password && confirmPassword && password.value === confirmPassword.value
+      ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Handle registration logic here
+      console.log('Registration submitted', this.registerForm.value);
+    }
+  }
+}
